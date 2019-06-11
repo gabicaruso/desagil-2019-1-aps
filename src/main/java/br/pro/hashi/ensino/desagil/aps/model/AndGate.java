ab7e8e8d327726ce67c1d1786f27f65cbf04979f
Gabriela Caruso <gabriela.caruso@yahoo.com.br>
2019-06-11 18:43:09 -0300

    aps1

---
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/AndGate.java
@@ -0,0 +1,35 @@
+package br.pro.hashi.ensino.desagil.aps.model;
+
+public class AndGate extends Gate {
+    private final NandGate nandA;
+    private final NandGate nandB;
+
+
+    public AndGate() {
+        super(1);
+
+        nandA = new NandGate();
+        nandB = new NandGate();
+    }
+
+
+    @Override
+    public boolean read() {
+        return nandB.read();
+    }
+
+
+    @Override
+    public void connect(int inputPin, SignalEmitter emitter) {
+        if (inputPin < 0 || inputPin > 1) {
+            throw new IndexOutOfBoundsException(inputPin);
+        } else if (inputPin == 0) {
+            nandA.connect(0, emitter);
+        } else if (inputPin == 1) {
+            nandA.connect(1, emitter);
+        }
+
+        nandB.connect(0, nandA);
+        nandB.connect(1, nandA);
+    }
+}
